738496690192d2c1f1c012b405eee0f9188d0438
Cedric Fournier <cf11157701@gmail.com>
2023-12-15 12:03:52 +0100

    ajout fontsize email

--- src/compoments/Presentation/index.jsx
+++ src/compoments/Presentation/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Link, Typography } from "@mui/material"
 import imgdev from '../../assets/img/img_dev.svg'
 
 function Presentation() {
@@ -16,23 +16,25 @@ function Presentation() {
         <Box textAlign={'center'}
         >
           <Typography 
-            fontSize={{ xs: "25px", sm: '35px', md: "40px", lg: "45px", xl: '55px' }} 
+            fontSize={{ xs: "25px", sm: '35px', md: "40px", lg: "45px" }} 
             component="h2" 
             marginTop={'12px'}
           >
             Bonjour, <span className="hand">🤚</span> je suis Cédric Fournier
           </Typography>
           <Typography 
-            fontSize={{ xs: "1rem", sm: '25px', md: "30px", lg: '35px', xl: '45px' }} 
+            fontSize={{ xs: "1rem", sm: '25px', md: "30px", lg: '35px' }} 
             component="h3" 
             margin={{ xs: "10px" }}
           >
           Développeur Frontend React
           </Typography>
-          <a href="mailto:cedricfournierwebdeveloper@gmail.com">cedricfournierwebdeveloper@gmail.com</a>
+          <Link fontSize={{ xs: '16px', md: '20px'}} href="mailto:cedricfournierwebdeveloper@gmail.com" >
+            cedricfournierwebdeveloper@gmail.com
+          </Link>
         </Box>
         <Box component={'img'} 
-          height={{ xs: '300px', sm: '400px',  md: '500px', lg: '600px', xl: '800px' }} 
+          height={{ xs: '300px', sm: '400px',  md: '500px', lg: '600px' }} 
           src={imgdev} 
           alt="Dessin d'un développeur"
         />
